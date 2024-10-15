c422550beb26a7dd68fe6d6ff85a67d2744005aa
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-10-15 21:19:56 +0800

    Pseudocode

---
+++ find_the_highest_number.py
@@ -0,0 +1,21 @@
+#Define variables by asking user to input 5 numbers
+
+#Compare num1 to other variables
+
+#If num1 is greater than other variables, print num1
+
+#Compare num2 to other variables
+
+#If num2 is greater than other variables, print num2
+
+#Compare num3 to other variables
+
+#If num3 is greater than other variables, print num3
+
+#Compare num4 to other variables
+
+#If num4 is greater than other variables, print num4
+
+#Compare num5 to other variables
+
+#If num5 is greater than other variables, print num5
